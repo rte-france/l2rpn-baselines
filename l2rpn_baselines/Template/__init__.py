b4c667c5359d6f1604499e20a82a77a325d83b08
Tezirg <jean.grizet@gmail.com>
2020-04-16 11:28:39 +0200

    Template __init__ docstring

--- l2rpn_baselines/Template/__init__.py
+++ l2rpn_baselines/Template/__init__.py
@@ -13,11 +13,11 @@ In the __init__ file, it is expected to export 3 classes with names that depends
 For example, say you chose to write a baseline with the awesome name "XXX" (what an imagination!) you should export
 in this __init__.py file:
 
-- `XXXBaseline` [**mandatory**] contains the definition of your baseline. It must follow the directives
+- `XXX` [**mandatory**] contains the definition of your baseline. It must follow the directives
    given in "Template.py"
-- `XXXBaseline_eval` [**mandatory**] contains the script to evaluate the performance of this baseline. It must
+- `evaluate` [**mandatory**] contains the script to evaluate the performance of this baseline. It must
   follow the directive in "evaluate.py"
-- `XXXBaseline_train` [**optional**] contains the script to train your baseline. If provided, it must follow
+- `train` [**optional**] contains the script to train your baseline. If provided, it must follow
   the directives given in "train.py"
   
 See the import above for an example on how to export your scripts properly.
