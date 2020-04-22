5a88580af192095add7cc6e4e8e45b7be86e40f6
Tezirg <jean.grizet@gmail.com>
2020-04-22 17:11:02 +0200

    DQN increase learning rate maybe?

--- l2rpn_baselines/DoubleDuelingDQN/train.py
+++ l2rpn_baselines/DoubleDuelingDQN/train.py
@@ -25,7 +25,7 @@ DEFAULT_PRE_STEPS = 256
 DEFAULT_TRAIN_STEPS = 1024
 DEFAULT_N_FRAMES = 4
 DEFAULT_BATCH_SIZE = 32
-DEFAULT_LR = 2e-5
+DEFAULT_LR = 1e-4
 
 def cli():
     parser = argparse.ArgumentParser(description="Train baseline DDQN")
