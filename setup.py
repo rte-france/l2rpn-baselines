f874c397dcf5d0b266439842ce8ceb69c97b1841
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2020-04-23 16:39:26 +0200

    minor fix

--- setup.py
+++ setup.py
@@ -37,11 +37,12 @@ pkgs = {
 
 
 setup(name='l2rpn_baselines',
-      version='0.1.0',
-      description='An environment that allows to perform powergrid optimization.',
-      long_description='Built with modularity in mind, this package allows to perform the same operations '
-                       'independently of the software used to compute powerflow or method to generate grid '
-                       'states or forecasts.',
+      version='0.1.1',
+      description='L2RPN Baselines a repository to host baselines for l2rpn competitions.',
+      long_description='This repository aims at facilitating the use of state of the art algorithm in coming from the '
+                       'reinforcement learning community or the power system community in the l2rpn competitions. It '
+                       'also provides some usefull function to make life or participants to the l2rpn competitions '
+                       'easier.',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Programming Language :: Python :: 3.6',
