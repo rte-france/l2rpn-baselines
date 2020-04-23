90e50586543350e286c107cc4532c475602e80e5
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2020-04-23 11:44:25 +0200

    adding a utility to zip file for codalab more easily

--- l2rpn_baselines/utils/__init__.py
+++ l2rpn_baselines/utils/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ["cli_eval", "cli_train", "str2bool", "save_log_gif"]
+__all__ = ["cli_eval", "cli_train", "str2bool", "save_log_gif", "zip_for_codalab"]
 
 from l2rpn_baselines.utils.cli_eval import cli_eval
 from l2rpn_baselines.utils.cli_train import cli_train
 from l2rpn_baselines.utils.str2bool import str2bool
 from l2rpn_baselines.utils.save_log_gif import save_log_gif
+from l2rpn_baselines.utils.zip_for_codalab import zip_for_codalab
