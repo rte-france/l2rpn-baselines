a4edcee72f7126310a797b530e42ae2e71b17d78
BDonnot <benjamin.donnot@gmail.com>
2020-06-05 12:39:41 +0200

    adding a proper docs for the packages

--- setup.py
+++ setup.py
@@ -17,7 +17,11 @@ pkgs = {
     ],
     "extras": {
         "docs": [
-            "grid2op[docs]"
+            "numpydoc>=0.9.2",
+            "sphinx>=2.4.4",
+            "sphinx-rtd-theme>=0.4.3",
+            "sphinxcontrib-trio>=1.1.0",
+            "autodocsumm>=0.1.13"
         ]
     }
 }
