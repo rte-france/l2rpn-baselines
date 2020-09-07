11aafac3a1d57e18a6aa36f331bab7eae6bd864a
mjothy <mario.jothy@artelys.com>
2020-09-07 13:48:11 +0200

    Fix forced printer constructor

--- l2rpn_baselines/ExpertAgent/main.py
+++ l2rpn_baselines/ExpertAgent/main.py
@@ -2,5 +2,5 @@ import grid2op
 from l2rpn_baselines.ExpertAgent import evaluate
 
 
-env = grid2op.make("D:\\RTE\\ExpertOp4Grid\\1 - Développement\\ExpertOp4Grid\\alphaDeesp\\ressources\\parameters\\l2rpn_2019_ltc_9")
+env = grid2op.make("D:\\Projets\\RTE\\ExpertOp4Grid\\alphaDeesp\\ressources\\parameters\\l2rpn_neurips_2020_track1_val")
 res = evaluate(env)
