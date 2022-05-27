c32abde5e498867629b8c4f05e45349aecedba4d
Gaëtan Serré <56162277+gaetanserre@users.noreply.github.com>
2022-05-27 13:12:06 +0200

    Default value for obs_space_kwargs and act_space_kwargs

--- l2rpn_baselines/PPO_SB3/evaluate.py
+++ l2rpn_baselines/PPO_SB3/evaluate.py
@@ -27,8 +27,8 @@ def evaluate(env,
              save_gif=False,
              gymenv_class=GymEnv,
              gymenv_kwargs=None,
-             obs_space_kwargs={},
-             act_space_kwargs={},
+             obs_space_kwargs=None,
+             act_space_kwargs=None,
              iter_num=None,
              **kwargs):
     """
@@ -149,6 +149,11 @@ def evaluate(env,
             env.close()
 
     """
+    
+    if obs_space_kwargs is None:
+        obs_space_kwargs = {}
+    if act_space_kwargs is None:
+        act_space_kwargs = {}
 
     # load the attributes kept
     my_path = os.path.join(load_path, name)
