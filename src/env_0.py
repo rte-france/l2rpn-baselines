acf139234376a74533dd77a45fc6b4d4612ddf13
Benedikt Schesch <b.schesch@gmail.com>
2023-11-02 15:41:11 -0700

    Best solution 2D grid

--- src/env_0.py
+++ src/env_0.py
@@ -8,8 +8,11 @@ from grid2op.gym_compat import GymActionSpace
 from ray.tune.registry import register_env
 from ray.rllib.utils.spaces.repeated import Repeated
 from gymnasium import Env
+import matplotlib.pyplot as plt
 from gymnasium import spaces
+import io
 import numpy as np
+from PIL import Image
 from gymnasium import spaces
 from collections import defaultdict
 import torch
@@ -19,45 +22,69 @@ from collections import OrderedDict
 
 class TestEnv(Env):
     def __init__(self) -> None:
-        
         super().__init__()
         self.bound = 10
+        self.n_dim = 2
+        self.n_agents = 1
         self.observation_space = spaces.Box(
-            low=-2*self.bound,
-            high=2*self.bound,
-            shape=(3,),
+            low=-3*self.bound,
+            high=3*self.bound,
+            shape=(3, self.n_dim,),
         )
         self.action_space = spaces.Box(
             low=-1,
             high=1,
-            shape=(1,),
+            shape=(self.n_dim,),
         )
 
     def observe(self):
-        obs = np.concatenate(
-            [
-                self.curr_state-self.target_state,
-                self.target_state,
-                self.curr_state,
-            ]
-        )
+        obs = np.stack([
+            self.curr_state - self.target_state,
+            self.target_state,
+            self.curr_state,
+        ])
+        assert self.observation_space.contains(obs)
         return obs
 
-    def reset(
-        self, *, seed: int | None = None, options: dict[str, Any] | None = None
-    ) -> tuple[Any, dict[str, Any]]:
-        self.target_state = np.random.uniform(low=-self.bound, high=self.bound, size=(1,))
-        self.curr_state = np.zeros_like(self.target_state)
+    def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[Any, dict[str, Any]]:
+        self.target_state = np.random.uniform(low=-self.bound, high=self.bound, size=(self.n_dim,)).astype(np.float32)
+        self.curr_state = np.zeros_like(self.target_state).astype(np.float32)
         self.n_steps = 0
-        return self.observe()
+        return self.observe(), {}
 
     def step(self, action: Any):
+        initial_distance = np.linalg.norm(self.curr_state - self.target_state)
         self.curr_state += action
-        self.curr_state = np.clip(self.curr_state, -self.bound, self.bound)
-        distance_to_target = np.abs(self.curr_state - self.target_state)
-        reward = -float(distance_to_target)
+        self.curr_state = np.clip(self.curr_state, -2*self.bound, 2*self.bound)
+        new_distance = np.linalg.norm(self.curr_state - self.target_state)
+        reward = initial_distance - new_distance
+        # reward = min(2*reward,reward)
         self.n_steps += 1
-        return self.observe(), reward, self.n_steps >= 100, {}
+        return self.observe(), reward, self.n_steps >= 100, False, {}
+
+
+    def render(self, mode='human'):
+        fig, ax = plt.subplots(tight_layout=True)
+        ax.set_xlim(-3 * self.bound, 3 * self.bound)
+        ax.set_ylim(-3 * self.bound, 3 * self.bound)
+
+        # Draw target state
+        ax.scatter(self.target_state[0], self.target_state[1], c='red', label='Target')
+
+        # Draw current state
+        ax.scatter(self.curr_state[0], self.curr_state[1], c='blue', label='Agent')
+
+        ax.legend()
+
+        if mode == 'human':
+            plt.show()
+        elif mode == 'rgb_array':
+            buf = io.BytesIO()
+            plt.savefig(buf, format='png')
+            buf.seek(0)
+            img_arr = np.array(Image.open(buf))
+            plt.close(fig)
+            return img_arr
 
 
 def env_creator(env_config):
---
+++ src/visualize.py
@@ -0,0 +1,37 @@
+from ray.rllib.algorithms.algorithm import Algorithm
+import env_0
+import agent_0
+import imageio
+from tqdm import tqdm
+
+my_new_ppo = Algorithm.from_checkpoint("/home/scheschb/ray_results/PPO_2023-11-02_15-33-22/PPO_test_env_d47b9_00000_0_2023-11-02_15-33-22/checkpoint_000002")
+env = env_0.TestEnv()
+
+obs, _ = env.reset()
+frames = []
+rewards = []
+for i in tqdm(range(100)):
+    action = my_new_ppo.compute_single_action(obs)
+    obs, reward, done, terminated, info = env.step(action)
+    rewards.append(reward)
+    frames.append(env.render(mode="rgb_array"))
+    if done:
+        break
+print("RL Reward:", sum(rewards))
+imageio.mimsave("movie.gif", frames)
+print("Done")
+
+obs, _ = env.reset()
+frames = []
+rewards = []
+for i in tqdm(range(100)):
+    # action = my_new_ppo.compute_single_action(obs)
+    action = -2.0*(obs>=0)+1.0
+    obs, reward, done, terminated, info = env.step(action)
+    rewards.append(reward)
+    frames.append(env.render(mode="rgb_array"))
+    if done:
+        break
+imageio.mimsave("movie_expert.gif", frames)
+print("Expert Reward:", sum(rewards))
+print("Done")
